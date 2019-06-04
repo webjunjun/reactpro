d9003bb87d995676a8fd2b833a6c00350ec407ba
webjunjun <975502845@qq.com>
2019-06-04 19:08:48 +0800

    修改

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './assets/css/reset.css';
+import App from './page/home/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
