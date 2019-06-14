6a3576bfee9eacb7ddec976c7d7e3c73cc8653c8
webjunjun <meliujun@qq.com>
2019-06-14 09:37:48 +0800

    优化

--- src/page/about/About.js
+++ src/page/about/About.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
 class About extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      date: new Date(),
+    }
+  }
+
   render() {
     return (
-      <h1>about</h1>
+      <h1>{this.state.date.toLocaleTimeString()}</h1>
     );
   }
 }
