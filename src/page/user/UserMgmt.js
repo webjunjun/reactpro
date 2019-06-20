1a52509b0b3f42f2c8f3860c764b3d4d97e65c14
webjunjun <975502845@qq.com>
2019-06-20 14:58:51 +0800

    功能开发

--- src/page/user/UserMgmt.js
+++ src/page/user/UserMgmt.js
@@ -1,12 +1,54 @@
 import React from 'react';
-import { Table } from 'antd';
+import { Table, Popconfirm, message, Modal } from 'antd';
 
 class UserMgmt extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      visible: false
+    };
+    this.confirmDel = this.confirmDel.bind(this);
+    this.cancelDel = this.cancelDel.bind(this);
+    this.showModal = this.showModal.bind(this);
+    this.handleOk = this.handleOk.bind(this);
+    this.handleCancel = this.handleCancel.bind(this);
+  }
+
+  showModal() {
+    this.setState({
+      visible: true,
+    });
+  }
+
+  handleOk() {
+    this.setState({
+      visible: false,
+    });
+  }
+
+  handleCancel() {
+    this.setState({
+      visible: false,
+    });
+  }
+
+  confirmDel() {
+    message.success('confirmDel');
+  }
+
+  cancelDel() {
+    message.error('cancelDel');
+  }
 
   render() {
     const columns = [{
       title: "序号",
-      dataIndex: "number"
+      dataIndex: "number",
+      sorter: (a, b) => {
+        return a.number - b.number
+      },
+      sortDirections: ['descend', 'ascend']
     }, {
       title: "姓名",
       dataIndex: "name"
@@ -25,28 +67,62 @@ class UserMgmt extends React.Component {
       render: (text, record) => {
         return (
           <span>
-            <a href="javascript:;">编辑</a>
-            <a href="javascript:;">删除</a>
+            <a href="javascript:;" onClick={this.showModal}>编辑</a>
+            &nbsp;&nbsp;
+            <Popconfirm
+              title="确定删除该用户吗？"
+              onConfirm={this.confirmDel}
+              onCancel={this.cancelDel}
+              okText="确定"
+              cancelText="取消"
+            >
+              <a href="javascript:;">删除</a>
+            </Popconfirm>
+            &nbsp;&nbsp;
+            <Popconfirm
+              title="确定停用该用户吗？"
+              onConfirm={this.confirmDel}
+              onCancel={this.cancelDel}
+              okText="确定"
+              cancelText="取消"
+            >
+              <a href="javascript:;">停用</a>
+            </Popconfirm>
           </span>
         )
       }
     }];
     const data = [{
-      number: '1',
+      number: 1,
       name: "刘军",
       mobile: "15629047169",
       mail: "975502845@qq.com",
       role: '超级管理员',
     },{
-      number: '2',
+      number: 2,
       name: "刘军",
       mobile: "15629047169",
       mail: "975502845@qq.com",
-      role: '超级管理员',
+      role: ' 管理员',
     }];
     return (
       <div>
-        <Table columns={columns} dataSource={data} />       
+        <Table rowKey="number" bordered columns={columns} dataSource={data} />
+        <Modal
+          title="编辑用户"
+          visible={this.state.visible}
+          onOk={this.handleOk}
+          onCancel={this.handleCancel}
+          okText="确定"
+          cancelText="取消"
+        >
+          <div>
+          姓名
+          手机号
+          邮箱
+          角色
+          </div>
+        </Modal>
       </div>
     );
   }
