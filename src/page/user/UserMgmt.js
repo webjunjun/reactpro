49159c1e69ca3a9a325775894134e0d2e193e901
webjunjun <meliujun@qq.com>
2019-06-21 15:01:14 +0800

    用户管理功能

--- src/page/user/UserMgmt.js
+++ src/page/user/UserMgmt.js
@@ -1,8 +1,54 @@
 import React from 'react';
-import { Table, Popconfirm, message, Modal } from 'antd';
+import { Row, Col, Button, Icon,Table, Popconfirm, message, Modal, Form, Input, Select } from 'antd';
+
+// 表单
+const EditUserForm = Form.create({name: "edit_user_form"})(
+  class extends React.Component {
+    render() {
+      const { form } = this.props;
+      const { getFieldDecorator } = form;
+      const formItemLayout = {
+        labelCol: {
+          sm: { span: 4 }
+        },
+        wrapperCol: {
+          sm: { span: 20 }
+        },
+      };
+      return (
+        <Form {...formItemLayout}>
+          <Form.Item label="姓名">
+            {getFieldDecorator('name', {
+              rules: [{ required: true, message: "请输入姓名" }],
+            })(<Input type="text" />)}
+          </Form.Item>
+          <Form.Item label="手机号">
+            {getFieldDecorator('mobile', {
+              rules: [{ required: true, message: "请输入手机号" }],
+            })(<Input type="tel" />)}
+          </Form.Item>
+          <Form.Item label="邮箱">
+            {getFieldDecorator('mail', {
+              rules: [{ required: true, message: "请输入邮箱" }],
+            })(<Input type="mail" />)}
+          </Form.Item>
+          <Form.Item label="角色">
+            {getFieldDecorator('role', {
+              rules: [{ required: true, message: "请选择角色" }],
+            })(<Select defaultValue="1">
+                <Option value="1">超级管理员</Option>
+                <Option value="2">高级管理员</Option>
+                <Option value="3">普通管理员</Option>
+              </Select>)}
+          </Form.Item>
+        </Form>
+      )
+    }
+  }
+);
+const Search = Input.Search;
 
 class UserMgmt extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {
@@ -22,8 +68,15 @@ class UserMgmt extends React.Component {
   }
 
   handleOk() {
-    this.setState({
-      visible: false,
+    const form = this.formRef.props.form;
+    form.validateFields((err, values) => {
+      if (err) {
+        return false;
+      }
+      form.resetFields();
+      this.setState({
+        visible: false,
+      });
     });
   }
 
@@ -40,6 +93,11 @@ class UserMgmt extends React.Component {
   cancelDel() {
     message.error('cancelDel');
   }
+
+  // 获取form里的数据，通过ref
+  saveFormRef = formRef => {
+    this.formRef = formRef;
+  };
 
   render() {
     const columns = [{
@@ -70,7 +128,7 @@ class UserMgmt extends React.Component {
             <a href="javascript:;" onClick={this.showModal}>编辑</a>
             &nbsp;&nbsp;
             <Popconfirm
-              title="确定删除该用户吗？"
+              title="确定删除该管理员吗？"
               onConfirm={this.confirmDel}
               onCancel={this.cancelDel}
               okText="确定"
@@ -80,7 +138,7 @@ class UserMgmt extends React.Component {
             </Popconfirm>
             &nbsp;&nbsp;
             <Popconfirm
-              title="确定停用该用户吗？"
+              title="确定停用该管理员吗？"
               onConfirm={this.confirmDel}
               onCancel={this.cancelDel}
               okText="确定"
@@ -105,25 +163,59 @@ class UserMgmt extends React.Component {
       mail: "975502845@qq.com",
       role: ' 管理员',
     }];
+    const rowSelection = {
+      hideDefaultSelections: true,
+      onChange: (selectedRowKeys, selectedRows) => {
+        console.log(`${selectedRowKeys}`);
+      },
+      selections: [{
+        key: "all",
+        text: "删除",
+        onSelect() {
+          // 
+        }
+      }]
+    }
     return (
-      <div>
-        <Table rowKey="number" bordered columns={columns} dataSource={data} />
-        <Modal
-          title="编辑用户"
-          visible={this.state.visible}
-          onOk={this.handleOk}
-          onCancel={this.handleCancel}
-          okText="确定"
-          cancelText="取消"
-        >
-          <div>
-          姓名
-          手机号
-          邮箱
-          角色
-          </div>
-        </Modal>
-      </div>
+      <Row>
+        <Col span={24}>
+          <Row style={{marginBottom: "20px"}}>
+            <Col span={2}>
+              <Button type="primary">
+                <Icon type="plus" />
+                新增用户
+              </Button>
+            </Col>
+            <Col span={12}>
+              <Search
+                placeholder="输入姓名"
+                enterButton="搜索"
+                size="large"
+                onSearch={value => console.log(value)}
+              />
+            </Col>
+          </Row>
+          <Row>
+          <Col span={24}>
+            <Table rowSelection={rowSelection} rowKey="number" bordered columns={columns} dataSource={data} />
+            <Modal
+              title="编辑用户"
+              visible={this.state.visible}
+              onOk={this.handleOk}
+              onCancel={this.handleCancel}
+              okText="确定"
+              cancelText="取消"
+            >
+              <div>
+                <EditUserForm
+                  wrappedComponentRef={this.saveFormRef}
+                ></EditUserForm>
+              </div>
+            </Modal>
+          </Col>
+          </Row>
+        </Col>
+      </Row>
     );
   }
 }
