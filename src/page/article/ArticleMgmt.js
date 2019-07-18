522e5de1705cc383ee3585d5d65c42f8f0467386
webjunjun <meliujun@qq.com>
2019-07-18 18:16:37 +0800

    文章

--- src/page/article/ArticleMgmt.js
+++ src/page/article/ArticleMgmt.js
@@ -1,50 +1,50 @@
 import React from 'react';
-import { Row, Col } from 'antd';
+import { Row, Col, Table } from 'antd';
 // 编辑器
 import BraftEditor from 'braft-editor';
 import 'braft-editor/dist/index.css';
 
 class ArticleMgmt extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      // 创建一个空的editorState作为初始值
-      editorState: BraftEditor.createEditorState(null)
-    };
-  }
-
-  // async componentDidMount () {
-  //   // 假设此处从服务端获取html格式的编辑器内容
-  //   const htmlContent = await fetchEditorContent();
-  //   // 使用BraftEditor.createEditorState将html字符串转换为编辑器需要的editorStat
-  //   this.setState({
-  //       editorState: BraftEditor.createEditorState(htmlContent);
-  //   })
-  // }
-
-  submitContent = async () => {
-    // 在编辑器获得焦点时按下ctrl+s会执行此方法
-    // 编辑器内容提交到服务端之前，可直接调用editorState.toHTML()来获取HTML格式的内容
-    const htmlContent = this.state.editorState.toHTML();
-    // const result = await saveEditorContent(htmlContent);
-  }
-
-  handleEditorChange = (editorState) => {
-    this.setState({ editorState });
-  }
 
   render() {
-    const { editorState } = this.state;
+    const dataSource = [
+      {
+        key: '1',
+        name: '胡彦斌',
+        age: 32,
+        address: '西湖区湖底公园1号',
+      },
+      {
+        key: '2',
+        name: '胡彦祖',
+        age: 42,
+        address: '西湖区湖底公园1号',
+      },
+    ];
+    
+    const columns = [
+      {
+        title: '姓名',
+        dataIndex: 'name',
+        key: 'name',
+      },
+      {
+        title: '年龄',
+        dataIndex: 'age',
+        key: 'age',
+      },
+      {
+        title: '住址',
+        dataIndex: 'address',
+        key: 'address',
+      },
+    ];
     return (
       <Row>
         <Col span={24}>
           <Row>
             <Col span={24}>
-                <BraftEditor
-                  value={editorState}
-                  onChange={this.handleEditorChange}
-                  onSave={this.submitContent}
-                />
+              <Table dataSource={dataSource} columns={columns} />;
             </Col>
           </Row>
         </Col>
