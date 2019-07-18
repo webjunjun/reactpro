ca9d03f174d33e0abaa391ddcaab5115e991c722
webjunjun <meliujun@qq.com>
2019-07-18 16:27:48 +0800

    页面

---
+++ src/page/discuss/DiscussMgmt.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Row, Col } from 'antd';
+
+class DiscussMgmt extends React.Component {
+
+  render() {
+    return (
+      <Row>
+        <Col span={24}>
+            我是评论
+        </Col>
+      </Row>
+    );
+  }
+}
+
+export default DiscussMgmt;
