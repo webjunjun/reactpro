af05b70cfd92f582419e3dc6a234cf0b9e26e656
webjunjun <975502845@qq.com>
2019-07-18 16:27:48 +0800

    页面

---
+++ src/page/theme/ThemeMgmt.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Row, Col } from 'antd';
+
+class ThemeMgmt extends React.Component {
+
+  render() {
+    return (
+      <Row>
+        <Col span={24}>
+            我是主题
+        </Col>
+      </Row>
+    );
+  }
+}
+
+export default ThemeMgmt;
