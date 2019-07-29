d9987b709ce862916634c8fd20a5a2e23a3816c2
webjunjun <meliujun@qq.com>
2019-07-29 11:42:19 +0800

    优化

--- src/components/FrontFooter.js
+++ src/components/FrontFooter.js
@@ -4,16 +4,16 @@ import React from 'react';
 class FrontFooter extends React.Component {
   render() {
     return (
-      <footer class="footer">
-        <div class="footer_wrap">
-          <p class="copyright">
+      <footer className="footer">
+        <div className="footer_wrap">
+          <p className="copyright">
             <span>记录生活，分享点滴 - Design By liujunn</span><br />
             <span>
               <a href="http://www.miitbeian.gov.cn/">鄂ICP备案15004902号</a> ©2015-2019
-                    </span>
+            </span>
           </p>
-          <div class="police">
-            <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=42090202000115"><img src="/skin/2019/imgs/police-beian.png" />
+          <div className="police">
+            <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=42090202000115"><img src={require('../assets/img/police-beian.png')} />
               <p>鄂公网安备 42090202000115号</p>
             </a>
           </div>
--- src/components/FrontHeader.js
+++ src/components/FrontHeader.js
@@ -4,17 +4,17 @@ import React from 'react';
 class FrontHeader extends React.Component {
   render() {
     return (
-      <div class="menu_wrap">
-        <div class="menu_inner">
-          <div class="logo">
+      <div className="menu_wrap">
+        <div className="menu_inner">
+          <div className="logo">
               <a href="/">博文天下</a>
           </div>
-          <ul class="menu_list">
-            <li class="menu_item active"><a href="/">首页</a></li>
-            <li class="menu_item"><a href="/web/">web前端</a></li>
-            <li class="menu_item"><a href="/projects/">项目</a></li>
-            <li class="menu_item"><a href="/essay/">日常</a></li>
-            <li class="menu_item"><a href="/about/">关于我</a></li>
+          <ul className="menu_list">
+            <li className="menu_item active"><a href="/">首页</a></li>
+            <li className="menu_item"><a href="/web/">web前端</a></li>
+            <li className="menu_item"><a href="/projects/">项目</a></li>
+            <li className="menu_item"><a href="/essay/">日常</a></li>
+            <li className="menu_item"><a href="/about/">关于我</a></li>
           </ul>
         </div>
       </div>
