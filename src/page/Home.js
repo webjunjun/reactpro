dfc1912a79166a59478dfde2be5ecaf879a075a8
webjunjun <meliujun@qq.com>
2019-07-30 17:51:27 +0800

    优化

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import FrontHeader from '../components/FrontHeader';
 import FrontFooter from '../components/FrontFooter';
-import { Layout } from 'antd';
-const { Footer, Content } = Layout;
 
 class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user_info: {}
+    }
+  }
+
+  componentDidMount() {
+    fetch("https://api.example.com/items")
+      .then(res => res.json())
+      .then((result) => {
+          this.setState({
+            isLoaded: true,
+            items: result.items
+          });
+        },(error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      );
+  }
+
   render() {
     return (
       <React.Fragment>
