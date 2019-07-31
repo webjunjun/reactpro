3c0d2d95f97e91a294285a75d45ee303f5a3660a
webjunjun <975502845@qq.com>
2019-07-31 16:53:58 +0800

    登录

--- src/page/Login.js
+++ src/page/Login.js
@@ -1,5 +1,54 @@
 import React from 'react';
-import { Row, Col } from 'antd';
+import { Row, Col, Form, Icon, Input, Button, Checkbox } from 'antd';
+
+const LoginForm = Form.create({name: "login_form"})(
+  class extends React.Component {
+    handleSubmit = (e) => {
+      e.preventDefault();
+      this.props.form.validateFields((err, values) => {
+        if (!err) {
+          console.log('Received values of form: ', values);
+        }
+      });
+    };
+    render() {
+      const { getFieldDecorator } = this.props.form;
+      return (
+        <Form onSubmit={this.handleSubmit} className="login-form">
+          <Form.Item>
+            {getFieldDecorator('username', {
+              rules: [{ required: true, message: '请输入用户名' }],
+            })(
+              <Input
+                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                placeholder="Username"
+              />,
+            )}
+          </Form.Item>
+          <Form.Item>
+            {getFieldDecorator('password', {
+              rules: [{ required: true, message: '请输入密码' }],
+            })(
+              <Input
+                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                type="password"
+                placeholder="Password"
+              />,
+            )}
+          </Form.Item>
+          <Form.Item>
+            {getFieldDecorator('remember', {
+              valuePropName: 'checked',
+              initialValue: true,
+            })(<Checkbox>自动登录</Checkbox>)}
+            <a className="login_form_forgot" href="javascript:;">忘记密码</a>
+            <Button type="primary" htmlType="submit" className="login_form_button">登录</Button> <a href="javascript:;">立即注册</a>
+          </Form.Item>
+        </Form>
+      )
+    }
+  }
+);
 
 class Login extends React.Component {
   render() {
@@ -7,7 +56,9 @@ class Login extends React.Component {
       <React.Fragment>
         <Row>
           <Col span={24}>
-            我是登录
+            <div className="login_box">
+              <LoginForm></LoginForm>
+            </div>
           </Col>
         </Row>
       </React.Fragment>
