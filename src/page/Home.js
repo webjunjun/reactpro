bff490c1ce2ce171b05f529a43a2927dcc618dd8
webjunjun <meliujun@qq.com>
2019-08-06 14:57:35 +0800

    前端页面

--- src/page/Home.js
+++ src/page/Home.js
@@ -43,6 +43,27 @@ class Home extends React.Component {
           <div className="article_inner">
             {articleItems}
           </div>
+          <div className="side_inner">
+            <div className="side_box">
+              <form className="search_box" action="/e/search/index.php" method="post">
+                <input type="text" name="keyboard" placeholder="搜索..." className="search_input" autocomplete="off" />
+                <button className="search_btn" type="submit">
+                    <i className="iconfont icon-search"></i>
+                </button>
+              </form>
+            </div>
+            <div className="side_box">
+              <h3 className="aside_title">
+                <i className="iconfont icon-shuqian"></i>
+                <span>文章推荐</span>
+              </h3>
+              <ul className="tuijian_list">
+                <li className="tuijian_item text-tradition">
+                  <a href="/essay/10.html" title="《书虫》——适合初学者的英语读物">《书虫》——适合初学者的英语读物</a>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
         <FrontFooter></FrontFooter>
       </React.Fragment>
