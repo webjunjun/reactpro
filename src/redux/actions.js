f82ef1d5cdb8ba703df4280ea2e0d0e6f8fe4ad3
webjunjun <975502845@qq.com>
2019-08-20 18:32:50 +0800

    学习redux

---
+++ src/redux/actions.js
@@ -0,0 +1,20 @@
+import { ADD_TODO, TOGGLE_TODO, SET_VISIBILITY_FILTER } from './actionTypes';
+
+const addTodo = (text) => {
+    return { type: ADD_TODO, text }
+}
+
+const toggleTodo = (text) => {
+    return { type: TOGGLE_TODO, text }
+}
+
+const setVisibilityFilter = (filter) => {
+    return { type: SET_VISIBILITY_FILTER, filter }
+}
+
+// action描述用户做了什么操作，触发reducer。
+export default {
+    addTodo,
+    toggleTodo,
+    setVisibilityFilter
+};
---
+++ src/redux/constants.js
@@ -0,0 +1,12 @@
+/**
+ * 普通常量
+ */
+const VisibilityFilters = {
+    SHOW_ALL: 'SHOW_ALL',
+    SHOW_COMPLETED: 'SHOW_COMPLETED',
+    SHOW_ACTIVE: 'SHOW_ACTIVE'
+}
+
+export default {
+    VisibilityFilters
+};
