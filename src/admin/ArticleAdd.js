c02d392d8f3eb643bb61ed6ed16c1d6706a40949
webjunjun <meliujun@qq.com>
2019-12-24 16:07:36 +0800

    优化

--- src/admin/ArticleAdd.js
+++ src/admin/ArticleAdd.js
@@ -2,7 +2,7 @@ import React from 'react';
 import AdminHeader from '../components/AdminHeader';
 import AdminSider from '../components/AdminSider';
 import AdminFooter from '../components/AdminFooter';
-import { Layout, Row, Col, Input, Select, Button } from 'antd';
+import { Layout, Row, Col, Form, Input, Select, Button } from 'antd';
 // 编辑器
 import BraftEditor from 'braft-editor';
 import 'braft-editor/dist/index.css';
@@ -10,13 +10,100 @@ const { Content } = Layout;
 const { Option } = Select;
 const { TextArea } = Input;
 
+const ArticleForm = Form.create({name: "article_form"})(
+  class extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        // 创建一个空的editorState作为初始值
+        editorState: BraftEditor.createEditorState(null)
+      };
+    }
+
+    handleSubmit = (e) => {
+      e.preventDefault();
+      // 
+    };
+    render() {
+      const { editorState } = this.state;
+      const { getFieldDecorator } = this.props.form;
+      const formItemLayout = {
+        labelCol: {
+          xs: { span: 24 },
+          sm: { span: 2 },
+        },
+        wrapperCol: {
+          xs: { span: 24 },
+          sm: { span: 10 },
+        },
+      };
+      return (
+        <Form className="column-form" onSubmit={this.handleSubmit}>
+          <Form.Item label="文章标题" {...formItemLayout}>
+            {getFieldDecorator('article-title', {
+              rules: [{ required: true, message: '请输入文章标题' }],
+            })(
+              <Input placeholder="文章标题" allowClear maxLength="30" />,
+            )}
+          </Form.Item>
+          <Form.Item label="文章关键字" {...formItemLayout}>
+            {getFieldDecorator('article-keywords', {
+              rules: [{ required: true, message: '请输入文章关键字' }],
+            })(
+              <TextArea rows={4} placeholder="文章关键字 - keywords" maxLength="200" />,
+            )}
+          </Form.Item>
+          <Form.Item label="作者" {...formItemLayout}>
+            {getFieldDecorator('article-author', {
+              rules: [{ required: true, message: '请输入作者' }],
+            })(
+              <Input placeholder="作者" allowClear maxLength="30" />,
+            )}
+          </Form.Item>
+          <Form.Item label="栏目" {...formItemLayout}>
+            {getFieldDecorator('column-type', {
+              initialValue: '1',
+            })(
+              <Select mode="multiple" placeholder="请选择栏目">
+                <Option value="1">栏目1</Option>
+                <Option value="2">栏目2</Option>
+                <Option value="3">栏目3</Option>
+              </Select>,
+            )}
+          </Form.Item>
+          <Form.Item label="Tags" {...formItemLayout}>
+            {getFieldDecorator('column-type', {
+              initialValue: '1',
+            })(
+              <Select mode="multiple" placeholder="请选择tags">
+                <Option value="1">Tag1</Option>
+                <Option value="2">Tag2</Option>
+                <Option value="3">Tag3</Option>
+              </Select>,
+            )}
+          </Form.Item>
+          <Form.Item label="文章内容" {...formItemLayout}>
+            <BraftEditor
+              value={editorState}
+              onChange={this.handleEditorChange}
+              onSave={this.submitContent}
+            />
+          </Form.Item>
+          <Form.Item label="自动分页" {...formItemLayout}>
+            <Input placeholder="自动分页字数" value="3000" allowClear />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit">提交</Button>
+          </Form.Item>
+        </Form>
+      )
+    }
+  }
+);
+
 class ArticleAdd extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      // 创建一个空的editorState作为初始值
-      editorState: BraftEditor.createEditorState(null)
-    };
   }
 
   // async componentDidMount () {
@@ -40,7 +127,6 @@ class ArticleAdd extends React.Component {
   }
 
   render() {
-    const { editorState } = this.state;
     return (
       <Layout className="admin_back">
         <AdminSider></AdminSider>
@@ -52,51 +138,7 @@ class ArticleAdd extends React.Component {
                 <Row>
                   <Col span={24}>
                     <h1>新增文章</h1>
-                    <div className="flex-form-item">
-                      <label>文章标题：</label>
-                      <Input placeholder="文章标题" allowClear />
-                    </div>
-                    <div className="flex-form-item">
-                      <label>文章关键字：</label>
-                      <TextArea rows={4} placeholder="文章关键字 - keywords" maxLength="200" />
-                    </div>
-                    <div className="flex-form-item">
-                      <label>作者：</label>
-                      <Input placeholder="作者" allowClear />
-                    </div>
-                    <div className="flex-form-item">
-                      <label>栏目：</label>
-                      <Select mode="multiple" placeholder="请选择栏目">
-                        <Option value="1">栏目1</Option>
-                        <Option value="2">栏目2</Option>
-                        <Option value="3">栏目3</Option>
-                      </Select>
-                    </div>
-                    <div className="flex-form-item">
-                      <label>tags：</label>
-                      <Select mode="multiple" placeholder="请选择tags">
-                        <Option value="1">Tag1</Option>
-                        <Option value="2">Tag2</Option>
-                        <Option value="3">Tag3</Option>
-                      </Select>
-                    </div>
-                    <div className="flex-form-item">
-                      <label>文章内容：</label>
-                      <BraftEditor
-                        value={editorState}
-                        onChange={this.handleEditorChange}
-                        onSave={this.submitContent}
-                      />
-                    </div>
-                    <div className="flex-form-item">
-                      <label>自动分页：</label>
-                      <Input placeholder="自动分页字数" value="3000" />
-                    </div>
-                  </Col>
-                </Row>
-                <Row>
-                  <Col span={24}>
-                    <Button type="primary">提交</Button>
+                    <ArticleForm onSubmit={this.handleSubmit} wrappedComponentRef={this.saveFormRef}></ArticleForm>
                   </Col>
                 </Row>
               </Col>
